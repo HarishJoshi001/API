31491615a94bccb6c587bc39a6ec62d2a520ba6a
Harish Joshi <86139303+HarishJoshi001@users.noreply.github.com>
2025-02-15 23:54:02 +0545

    Add files via upload

---
+++ rest/src/main/java/com/harish/rest/controller/CalcController.java
@@ -0,0 +1,21 @@
+package com.harish.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CalcController {
+
+    @GetMapping("/add")
+    public double add(@RequestParam double num1, @RequestParam double num2) {
+        return num1 + num2;
+    }
+
+    @GetMapping("/subtract")
+    public double subtract(@RequestParam double num1, @RequestParam double num2) {
+        return num1 - num2;
+    }
+}
